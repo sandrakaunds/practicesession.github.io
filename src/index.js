56b703e2718cacdd2635aaba7ce71b4b1b5de6a7
sandrakaunds <sandrakaunds2021@gmail.com>
2022-05-20 15:54:34 +0530

    My Practice session
    Practice session

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+    <br />
+    <h2>Practice session May 2022</h2>
+    <p>@sandra kaunds 2022</p>
+  </div>,
+  document.getElementById("root")
+);
